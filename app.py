aecda581377bbc9e36def98fe4110729f7da9c28
Vectors <programming@vectors.net.au>
2018-05-21 22:01:52 +0800

    skeleton flask

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 #! /usr/bin/env python3.6
 
-from flask import Flask
+from flask import (Flask,
+                   render_template)
+
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+
+@app.route("/")
+def showMain():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
